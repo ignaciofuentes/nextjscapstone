ee3a306a4d0a652588507336299a05d08f8771af
Ignacio Fuentes <ignaciofuentes@outlook.com>
2024-03-08 11:14:58 -0500

    cleaning up UI

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -19,31 +19,61 @@ export default function TodoList() {
 
   useEffect(() => {
     const sub = client.models.Todo.observeQuery().subscribe(({ items }) =>
-        setTodos([...items])
+      setTodos([...items])
     );
 
     return () => sub.unsubscribe();
   }, []);
 
   return (
-    <div>
-      <h1>Todos</h1>
-      <button onClick={async () => {
-        // create a new Todo with the following attributes
-        const { errors, data: newTodo } = await client.models.Todo.create({
-          // prompt the user to enter the title
-          content: window.prompt("title"),
-          done: false,
-          priority: 'medium'
-        })
-        console.log(errors, newTodo);
-      }}>Create </button>
-
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
+    <div className="bg-gray-100 h-screen flex items-center justify-center">
+      <div className="w-full max-w-xs">
+        <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <div className="mb-4">
+            <h1 className="block text-gray-700 text-lg font-bold mb-2">
+              Todo List
+            </h1>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={async () => {
+                // create a new Todo with the following attributes
+                const { errors, data: newTodo } =
+                  await client.models.Todo.create({
+                    // prompt the user to enter the title
+                    content: window.prompt("title"),
+                    done: false,
+                    priority: "medium",
+                  });
+                console.log(errors, newTodo);
+              }}
+            >
+              Add new todo{" "}
+            </button>
+          </div>
+          <div>
+            <ul className="list-reset">
+              {todos.map((todo) => (
+                <li
+                  key={todo.id}
+                  className="flex justify-between items-center bg-gray-100 rounded mb-2 p-2"
+                >
+                  <span className="text-gray-700">{todo.content}</span>
+                  <div>
+                    <button
+                      onClick={async () => {
+                        await client.models.Todo.delete({ id: todo.id });
+                      }}
+                      className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded"
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
