3008706905870c81bd4c865e50f01340127a14b1
Monimoralesa <68556552+Monimoralesa@users.noreply.github.com>
2024-03-26 09:16:41 -0500

    Update page.tsx
    Added initial CodeWhisperer comments for Amplify app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,15 @@
 "use client";
 
 import TodoList from "@/components/TodoList";
+
+// import react authentication from amplify
 import { withAuthenticator } from "@aws-amplify/ui-react";
+
+// import aws-amplify css styles for react
 import "@aws-amplify/ui-react/styles.css";
 import NavBar from "@/components/NavBar";
 
+// create an App function that returns NavBar and TodoList
 function App() {
   return (
     <>
