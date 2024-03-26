5d3c3f757a46a66643384bede63f18012e74db5b
Monimoralesa <68556552+Monimoralesa@users.noreply.github.com>
2024-03-26 09:13:21 -0500

    Update layout.tsx
    Added initial CodeWhisperer comments for Amplify app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+// Import ConfigureAmplifyClientSide function. 
 import ConfigureAmplifyClientSide from "@/components/ConfigureAmplify";
 
 const inter = Inter({ subsets: ["latin"] });
